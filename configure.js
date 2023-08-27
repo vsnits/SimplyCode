80efa9c00b5245c5c0d6d47e1fe3b718e378e439
Vsevolod Nitsenko <hellovsnits@tutanota.com>
2023-08-27 11:01:10 +0300

    Update configure.js

--- configure.js
+++ configure.js
@@ -1,10 +1,13 @@
-
-    configure.cache = false
-    configure.cacheonfocus = false
-    configure.gridsize = 15
-    configure.gridcolor = [120,120,120,70]
-    configure.background = "rgb(255, 255, 255)"
-    configure.fontcolor = "rgb(22, 91, 92)"
+/* 
+    Note, wget does not captures documentation! Pleace use git clone command.
+    Repository link for more information: https://github.com/vsnits/SimplyCode/ 
+*/
+  configure.cache = false
+  configure.cacheonfocus = false
+  configure.gridsize = 15
+  configure.gridcolor = [120,120,120,70]
+  configure.background = "rgb(255, 255, 255)"
+  configure.fontcolor = "rgb(22, 91, 92)"
 
       
 
