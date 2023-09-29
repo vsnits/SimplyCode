3c9a08a7aa421e0187760738e2aa51b9687890a8
Vsevolod Nitsenko <hellovsnits@tutanota.com>
2023-09-29 16:08:37 +0300

    Update keys.js
    removed annoying enter tab

--- setup/keys.js
+++ setup/keys.js
@@ -1,5 +1,3 @@
-
-  // keys.js/Users/Data/Documents/Hobby/Code/JS/Apps/@SimplyCode!/git/setup/keys.js
   
   document.onkeydown=function(e) {
       if(e.code == "Space" && e.altKey) { putInside() }
@@ -11,7 +9,6 @@
       else if(e.code == "KeyI" && (e.ctrlKey || e.altKey)) { e.preventDefault(); uploadText() }
       else if(e.code == 'KeyQ' && e.altKey) { e.preventDefault(); input.value = JCtab(input.value, 4, 2) }
       else if(e.code == 'KeyA' && e.altKey) { e.preventDefault(); input.value = JCtab(input.value, 3, 1, "\n\n") }
-      else if(e.key == 'Enter') { e.preventDefault(); pustText(input,"\n" + "   ",0)  }
       else if(e.key == 'Tab') { e.preventDefault(); pustText(input, "    ",0) }
       else if(e.code == "KeyR" && e.altKey) { e.preventDefault(); randomStyle() }
       else if(e.code == "KeyH" && e.altKey) { e.preventDefault(); history.view() }
